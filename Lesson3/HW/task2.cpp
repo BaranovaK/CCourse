05728e3d991d2abc53f54679435b9aca81a485da
BaranovaK <32207800+BaranovaK@users.noreply.github.com>
2017-09-30 10:31:10 +0300

    Create task2.cpp

---
+++ Lesson3/HW/task2.cpp
@@ -0,0 +1,34 @@
+int main()
+{
+	
+		float a = 0;
+		float b = 0;
+		float a1 = 0;
+		float b1 = 0;
+		float x = 0;
+		float y = 0;
+
+		float result = 0;
+		printf("Enter a\n");
+		scanf_s("%f", &a);
+		printf("Enter b\n");
+		scanf_s("%f", &b);
+		printf("Enter x\n");
+		scanf_s("%f", &x);
+		printf("Enter y\n");
+		scanf_s("%f", &y);
+
+		if (a = a1, b = b1)
+		{
+			x = -(b - b1) / a - a1;
+			printf("x=(b-b1)/a-a1=%f\n", x);
+			y = -(b - b1) / a - a1;
+			printf("y = ((b - b1) / a - a1)*(a+b) = %f\n", y); 
+
+
+		}
+		else
+			printf("Error!\n");
+
+		return 0;
+	}
