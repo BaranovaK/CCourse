3e8fcedff1736f4adb122ed50903cec4e6e8003a
BaranovaK <32207800+BaranovaK@users.noreply.github.com>
2017-10-28 07:52:33 +0300

    Create task2.cpp

---
+++ Lesson_5/HW/task2.cpp
@@ -0,0 +1,18 @@
+int factorial(int base)
+{
+	int result = 1;
+	for ((n - 1)*factorial*n;)
+	result = result*i;
+		return result;
+}
+
+int main()
+{
+	int y = 0;
+	int result1 = 0;
+	printf("Enter y:\n");
+	scanf_s("%d", &y);
+	result1 = factorial(y);
+	printf("Result: %d\n", result1);
+	return 0;
+}
