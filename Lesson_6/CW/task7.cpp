5ed183586d3790f1389a59ddae8958cfa7d41f23
BaranovaK <32207800+BaranovaK@users.noreply.github.com>
2017-10-28 13:03:54 +0300

    Create task7.cpp

---
+++ Lesson_6/CW/task7.cpp
@@ -0,0 +1,39 @@
+//Программа поиска суммы и произведения в массиве с использованием динамического выделения памяти, 
+задания размера массива пользователем, а так же введения пользователем массива
+#include "stdafx.h"
+#include <stdlib.h>
+
+
+
+int main()
+
+{
+	
+	int  *ptr_v;
+	
+	int i = 0;
+	int N = 0;
+	scanf_s("%d", &N);
+	ptr_v = (int *)malloc(N * sizeof(int));
+
+	for (int i = 0; i < N; i++)
+		scanf_s("%d", (ptr_v + i));
+
+	for (int i = 0; i < N; i++)
+		printf("%d\n", *(ptr_v + i));
+
+	int a = 0;
+	for (int i = 0; i < N; i++)
+	a = a + ptr_v[i];
+
+		printf("%d\n", a);
+		
+		int b = 1;
+		for (int i = 0; i < N; i++)
+			b= b * ptr_v[i];
+
+		printf("%d\n", b);
+	
+		return 0;
+}
+
