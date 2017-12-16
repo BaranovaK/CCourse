983bd47118de8d5dd817727cda2ff587f42b0b12
BaranovaK <32207800+BaranovaK@users.noreply.github.com>
2017-12-16 10:55:35 +0300

    Create task1.cpp

---
+++ Lesson_13/CW/task1.cpp
@@ -0,0 +1,30 @@
+//Передача всей структуры в функцию, дораюотка программы, чтобы поля структуры заполнялисьс клавиатуры
+#include "stdafx.h"
+	struct student {
+		char name[20];
+		char surname[30];
+		float average_rating;
+		int birthday;
+		int id;
+	};
+	void print_student_info(struct student student_info)
+	{
+		printf("ID: %d\n", student_info.id);
+		printf("Name: %s\n", student_info.name);
+		printf("Surname: %s\n", student_info.surname);
+		printf("Rating: %f\n", student_info.average_rating);
+		printf("Birthday: %d\n", student_info.birthday);
+	}
+	int main()
+	{
+		struct student student_info;
+		scanf_s("%d", &student_info.id);
+		scanf_s("%20s", student_info.name,20);
+		scanf_s("%20s", student_info.surname,20);
+		scanf_s("%f", &student_info.average_rating);
+		scanf_s("%d", &student_info.birthday);
+
+		print_student_info(student_info);
+
+		return 0;
+	}
