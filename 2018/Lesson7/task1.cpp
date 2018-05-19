9e80c347d4eb00039c3543571e98be536b7fe370
BaranovaK <32207800+BaranovaK@users.noreply.github.com>
2018-05-19 11:01:59 +0300

    Create task1.cpp

---
+++ 2018/Lesson7/task1.cpp
@@ -0,0 +1,31 @@
+//Доделать программу излеции и посмотреть в какой последовательности будут выведены сообщения
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+class queue {
+	int _q[100];
+	int _sloc, _rloc;
+public:
+	queue(); // конструктор
+	~queue(); // деструктор
+	void gput(int i);
+	int qget();
+};
+queue::queue()
+{
+	_sloc = _rloc = 0;
+	printf("Queue initialized. \n");
+}
+queue ::~queue()
+{
+	printf("Queue destroyed.\n");
+}
+
+int main() {
+	queue MyQueue1;
+	queue MyQueue2;
+	return 0;
+}
